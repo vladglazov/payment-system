7f7af18cb711dcb85f2414a2eb5ed3672f32109d
vladglazov <vlad98glazov@gmail.com>
2025-09-06 10:09:38 +0300

    Use currency-specific commission rates

--- src/payment.js
+++ src/payment.js
@@ -11,3 +11,16 @@ function processPayment(amount, currency) {
 }
 
 module.exports = { processPayment };
+
+const rates = require('../config/rates.json');
+
+function processPayment(amount, currency) {
+    const commission = rates[currency] || 0.02;
+    const total = amount * (1 + commission);
+    return {
+        amount: amount,
+        commission: amount * commission,
+        total: total,
+        currency: currency
+    };
+}
